cd4fec83d24287329d7e8b023bb8ab7553ec5e8a
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2020-02-20 14:23:52 +0200

    makeda's first notes

--- src/Anon.ts
+++ src/Anon.ts
@@ -27,10 +27,46 @@ function randomUid() {
 };
 
 //CMHM new function to check that what is in fields is proper
-
-
-
-
+//below are fields that are kept
+// the psuedocode fot what I am going to add is this: 
+// if a field, for exmaple "00080060" contains ("CT" or "MRI" or "Xray" or "flouro" or "US" or "ultrasound" in the DICOM language- must check:
+//     "action":"keep"
+// else 
+// //     "action":"remove"
+// OBVIOUSLY I NEED TO CHECK that CT and MRI are not hashed or encoded or entered as Numbers or , or or,or
+// // "00080016": {"action":"keep", "description":"SOP Class UID"},
+// The following are additional fields to check:
+// "00080060": { "description":"Modality"},
+// "00080008": { "description":"Image Type"},
+// "00080005": {"description":"Specific Character Set"},
+// "00180095": { "description":"Pixel Bandwidth"},
+// "00280002": { "description":"Samples per Pixel"},
+// "00280004": { "description":"Photometric Interpretation"},
+// "00280010": { "description":"Rows"},
+// "00280011": { "description":"Columns"},
+// "00280030": { "description":"Pixel Spacing"},
+// "00280100": {"description":"Bits Allocated"},
+// "00280101": { "description":"Bits Stored"},
+// "00280103": {"description":"Pixel Representation"},
+// "00280106": {"description":"Smallest Image Pixel Value"},
+// "00280107": {" "description":"Largest Image Pixel Value"},
+// "00281050": {"description":"Window Center"},
+// "00281051": {"description":"Window Width"},
+// "00281052": {"description":"Rescale Intercept"},
+// "00281053": { "description":"Rescale Slope"},
+// "00281055": { "description":"Window Center & Width Explanation"},
+// "00180015": { "description":"Body Part Examined"},
+// "00180020": { "description":"Scanning Sequence"},
+// "00180021": { "description":"Sequence Variant"},
+// "00180022": { "description":"Scan Options"},
+// "00180050": { "description":"Slice Thickness"},
+// "00185100": { "description":"Patient Position"},
+// "00200032": { "description":"Image Position (Patient)"},
+// "00201041": { "description":"Slice Location"},
+// // and also lt's check about the actual pixel data. perhaps even in later versions for steganography
+// "00280102": { "description":"High Bit, required to decode pixel data"},
+// "7FE00010": { "description":"Pixel Data"}, 
+//
 // TODO: Test that there's no personal data stored outside the "Value" for a
 // given tag. This should be the case, and we're making the assumption that the
 // user is not maliciously trying to hide data.
