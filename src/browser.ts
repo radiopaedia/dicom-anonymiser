b3e4983c230aba90fcdc75e5159f4614d883b44f
Daniel Heath <daniel@heath.cc>
2020-07-21 11:46:19 +1000

    Avoid clobbering the window too much

--- src/browser.ts
+++ src/browser.ts
@@ -2,7 +2,5 @@ import * as dcmio from './index';
 import {saveAs} from "file-saver";
 if (typeof window != 'undefined') {
   // @ts-ignore
-  window.dcmio = dcmio
-  // @ts-ignore
-  window.saveAs = saveAs
+  window.dicomanon = {dcmio, saveAs}
 }
