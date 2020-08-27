b8378e522fa2d393ff61f7b3f18045ccf9fe9d58
Daniel Heath <daniel@heath.cc>
2020-08-27 17:32:06 +1000

    Fix vr types

--- src/ValueRepresentation.ts
+++ src/ValueRepresentation.ts
@@ -1,6 +1,7 @@
 import {
   WriteBufferStream,
-  ReadBufferStream
+  ReadBufferStream,
+  BufferStream
 } from './BufferStream';
 import DicomMessage from "./Message";
 import Tag from './Tag';
@@ -224,7 +225,7 @@ export class BinaryRepresentation extends ValueRepresentation {
                 frames = value.length;
             let startOffset: Array<number> = [];
 
-            var binaryStream = new WriteBufferStream(1024 * 1024 * 20, stream.isLittleEndian);
+            var binaryStream: BufferStream = new WriteBufferStream(1024 * 1024 * 20, stream.isLittleEndian);
             for (var i = 0;i < frames;i++) {
                 startOffset.push(binaryStream.size);
                 var frameBuffer = value[i], frameStream = new ReadBufferStream(frameBuffer),
